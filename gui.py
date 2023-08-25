43dc0ff56a4d9d40476bc698e6b7b45eb5856062
repeckadonatas <repeckadonatas@yahoo.comm>
2023-08-25 09:42:40 +0300

    Revert "A basic GUI without interactions"
    This reverts commit 048a7b1c89c51d74bd4bd13b7c6eb206ee207352.

--- gui.py
+++ gui.py
@@ -2,25 +2,9 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text('Type in a to-do')
-input_box = sg.InputText(tooltip='Enter a to-do', key='todo')
+input_box = sg.InputText(tooltip='Enter a to-do')
 add_button = sg.Button('Add')
 
-window = sg.Window('To-Do App',
-                   layout=[[label], [input_box, add_button]],
-                   font=('Helvetica', 10))
-
-while True:
-    event, values = window.read()
-    print(event)
-    print(values)
-    match event:
-        case 'Add':
-            todos = functions.get_todos()
-            new_todo = values['todo'] + '\n'
-            todos.append(new_todo)
-            functions.write_todos(todos)
-        case sg.WIN_CLOSED:
-            break
-
-
+window = sg.Window('To-Do App', layout=[[label], [input_box, add_button]])
+window.read()
 window.close()
